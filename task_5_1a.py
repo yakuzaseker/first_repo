fbb772bceb00fe08ac24d91423e77c2d92bc5525
yakuzaseker <inferno2070@gmail.com>
2025-12-19 01:08:38 +0600

    словари

---
+++ task_5_1a.py
@@ -0,0 +1,20 @@
+london_co = {
+    "r1": {
+        "location": "21 New Globe Walk",
+        "vendor": "Cisco",
+        "model": "4451",
+        "ios": "15.4",
+        "ip": "10.255.0.1",
+    },
+    "sw1": {
+        "location": "21 New Globe Walk",
+        "vendor": "Cisco",
+        "model": "3850",
+        "ios": "3.6.XE",
+        "ip": "10.255.0.101",
+    }
+}
+device_name = input("Введите имя устройства: ")
+parameter = input("Введите имя параметра: ")
+
+print(london_co[device_name][parameter])
